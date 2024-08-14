783531db5a6770237b9442d8ac8760d1da2e2918
Lost Girl <ekaterina_kryukova90@mail.ru>
2024-08-14 15:13:05 +0300

    Test commit 2

--- app/src/main/java/ru/lostgirl/testaction/MainActivity.kt
+++ app/src/main/java/ru/lostgirl/testaction/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $nam!1",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
